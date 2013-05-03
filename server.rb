8bf8df5488a55743c3c533aaab91208256c5324f
Tom Counsell <tom@counsell.org>
2013-05-03 23:31:47 +0100

    Make it easier to add elements to the url

--- server.rb
+++ server.rb
@@ -2,9 +2,16 @@ require 'sinatra'
 require 'json'
 require './model'
 
-# We don't care about standard FFI methods
+# We want to be able to work out which methods are unique to our Model
+# and which methods are common to all FFI modules, so we create an empty
+# FFI module.
 module FFIMethodsToIgnore; extend FFI::Library; end
 
+# This is used to work out what named references exist in the model
+# and then to assign them to one of three groups:
+# inputs - named references that are setable
+# series - named references that return an array
+# outputs - any other named references
 def extract_model_structure
   # Get all the excel references
   relevant_methods = (Model.methods - FFIMethodsToIgnore.methods - [:reset])
@@ -30,17 +37,35 @@ def extract_model_structure
   structure
 end
 
+# Only work out the model structure once
 model_structure = extract_model_structure()
 
-get '/data/1/:maximum_low_carbon_build_rate/:electricity_demand_in_2050' do 
+# This should be identical to the one in src/javacripts/chart.js.coffee
+url_structure = [
+  "version",
+  "maximum_low_carbon_build_rate",
+  "electricity_demand_in_2050"
+] - ["version"] # A cludge to make the above lines easier to copy and paste
+
+# This is the method that is used to request data from the model
+# the first part of the url is to match a version number the remainder
+# should match the url_structure above. 
+#
+# The method sets the named methods in the url structure to the values passed in the url
+# it then alters the year_second_wave_of_building_starts back from 2050 towards 2010 to
+# try and get 2050 emissions below 5gCO2/kWh.
+#
+# It then passes back the results of the model as json
+get '/data/1/*' do 
   m = ModelShim.new
-  build_constraint = params[:maximum_low_carbon_build_rate].to_f
-  electricity_demand_in_2050 = params[:electricity_demand_in_2050].to_f
+  # If a parameter looks like a number, make it a number
+  controls =  params[:splat][0].split('/').map { |v| v =~ /^\d+\.?\d*$/ ? v.to_f : v }
   year = 2050
   while year >= 2010
     m.reset
-    m.maximum_low_carbon_build_rate = build_constraint
-    m.electricity_demand_in_2050 = electricity_demand_in_2050
+    controls.each.with_index do |v,i|
+      m.send(url_structure[i]+"=",v)
+    end
     m.year_second_wave_of_building_starts = year
     break if m.emissions_factor_2050 < 5
     year = year - 1
