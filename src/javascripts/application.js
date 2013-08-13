e3f9a8c0bf908433dd4eb59cb91c808079efc9c4
Tom Counsell <tom@counsell.org>
2013-08-13 16:25:39 +0100

    Added dataset shim for IE9

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,6 +1,7 @@
 // This is a manifest file that'll be compiled into including all the files listed below.
 
 // First the support libs
+//= require html5-dataset
 //= require d3.min
 
 //= require chart
